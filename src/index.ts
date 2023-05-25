157c0d6e8457c942f4913de55513c68f415ff34c
Jack O'Brien <thejackobrien29@gmail.com>
2023-05-25 09:31:34 -0400

    add openAI haiku

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 #!/usr/bin/env node
 
 import inquirer from 'inquirer';
+import chalkAnimation from 'chalk-animation';
+
 import { welcome } from './utils/welcome.js';
 import { getRandomGreeting } from './utils/random.js';
+import { generateWelcomePoem } from './utils/openAI.js';
+
+async function problem() {
+  // solve the actual problem here
+}
 
 async function main() {
   const { name } = await inquirer.prompt([
@@ -13,7 +20,20 @@ async function main() {
     },
   ]);
 
+  problem();
+
+  console.log();
   console.log(`${getRandomGreeting()}, ${name}!`);
+  console.log();
+
+  const loading = chalkAnimation.neon('Loading Welcome Poem...');
+  const haiku = await generateWelcomePoem(name);
+  loading.replace('');
+  loading.stop();
+
+  console.log();
+  console.log(haiku);
+  console.log();
 }
 
 // For fun, can remove for the real thing!
---
+++ src/utils/openAI.ts
@@ -0,0 +1,34 @@
+import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
+
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const getPrompt = (name: string) => {
+  return `Write a haiku about a person named "${name}".
+    This is a welcome poem, so make it positive and show them that you're happy to meet them. 
+    Make the poem rythme and include puns that have to do with thier name.
+    Don't make assumptions about their gender, age, or anything else.`;
+};
+
+export const generateWelcomePoem = async (name: string) => {
+  try {
+    const completion = await openai.createChatCompletion({
+      model: 'gpt-4',
+      messages: [{ role: 'user', content: getPrompt(name) }],
+      temperature: 0.7,
+      max_tokens: 1000,
+    });
+
+    // console.info(completion.data.choices[0].message?.content);
+
+    return completion.data.choices[0].message?.content || null;
+  } catch (e) {
+    console.error(`Error creating completion for input ${getPrompt(name)}: ${e}`);
+    return '';
+  }
+};
